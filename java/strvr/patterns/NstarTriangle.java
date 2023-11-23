e18868cc232b2473f315397398c753b6955c7473
Shivam97.rs@gmail.com <crikfan5@gmail.com>
2023-11-23 12:13:25 -0500

    Patterns

---
+++ java/strvr/patterns/NstarTriangle.java
@@ -0,0 +1,18 @@
+package strvr.patterns;
+//@Link - https://www.codingninjas.com/studio/problems/star-triangle_6573671?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_patternproblems&leftPanelTabValue=PROBLEM
+//@Link - https://takeuforward.org/strivers-a2z-dsa-course/must-do-pattern-problems-before-starting-dsa/
+public class NstarTriangle {
+    public static void nStarTriangle(int n) {
+        // Write your code here
+        for(int i = 0; i<n; i++){
+            for(int j = 1; j<n-i; j++){
+                System.out.print(" ");
+            }
+
+            for(int k = 1; k<=(2*i +1); k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ java/strvr/patterns/Ntriangle.java
@@ -0,0 +1,15 @@
+package strvr.patterns;
+//@link - https://www.codingninjas.com/studio/problems/n-triangles_6573689?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_patternproblems&leftPanelTabValue=PROBLEM
+//@Link - https://takeuforward.org/strivers-a2z-dsa-course/must-do-pattern-problems-before-starting-dsa/
+public class Ntriangle {
+    public static void nTriangle(int n) {
+        // Write your code here
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=i; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
