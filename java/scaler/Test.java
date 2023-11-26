0c4f8f8df25613f09a5727e0e7f13206429714fc
Shivam97.rs@gmail.com <crikfan5@gmail.com>
2023-11-26 12:31:44 -0500

    Test

--- java/scaler/Test.java
+++ java/scaler/Test.java
@@ -1,4 +1,10 @@
 package scaler;
 
 public class Test {
+    public static void main(String[] args) {
+        int a = -32;
+        int c = a/10;
+        int b = Math.abs(a);
+        System.out.println(c);
+    }
 }
