fdb38b8c5750dfd6223c12aaee564e6eababec94
Shivam97.rs@gmail.com <crikfan5@gmail.com>
2023-11-26 18:03:25 -0500

    Palindrome

---
+++ java/strvr/basicmath/PalindromeNumber.java
@@ -0,0 +1,18 @@
+package strvr.basicmath;
+
+//@Link - https://www.codingninjas.com/studio/problems/palindrome-number_624662?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=PROBLEM
+//@Link - https://takeuforward.org/data-structure/check-if-a-number-is-palindrome-or-not/
+public class PalindromeNumber {
+    public static boolean palindromeNumber(int n){
+        // Write your code here.
+        String a = Integer.toString(n);
+        StringBuilder b = new StringBuilder();
+        b.append(a);
+        b.reverse();
+        if(a.equals(b.toString())){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
