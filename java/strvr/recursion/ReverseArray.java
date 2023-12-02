009e107737b641f2214644abdb536e0836772fa6
Shivam97.rs@gmail.com <crikfan5@gmail.com>
2023-12-01 23:06:30 -0500

    Reverse array using 2 pointer

---
+++ java/strvr/recursion/ReverseArray.java
@@ -0,0 +1,22 @@
+package strvr.recursion;
+//@Link - https://www.codingninjas.com/studio/problems/reverse-an-array_8365444?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=PROBLEM
+//@Link - https://takeuforward.org/data-structure/reverse-a-given-array/
+public class ReverseArray {
+    public static int[] reverseArray(int n, int []nums) {
+        // Write your code here.
+        //int[] output = new int[n];
+        swap(0,n-1,nums);
+        return nums;
+
+    }
+
+    public static void swap(int l, int r, int[] arr){
+        if(l>=r){
+            return;
+        }
+        int temp = arr[l];
+        arr[l] = arr[r];
+        arr[r] = temp;
+        swap(l+1,r-1,arr);
+    }
+}
