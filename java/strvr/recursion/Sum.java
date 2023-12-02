0c57d06bd9d880324275c6aec9d8773d82d6c9d3
Shivam97.rs@gmail.com <crikfan5@gmail.com>
2023-12-01 21:00:53 -0500

    Sum of 1st N natural number

---
+++ java/strvr/recursion/Sum.java
@@ -0,0 +1,19 @@
+package strvr.recursion;
+
+public class Sum {
+
+    public static long sumFirstN(long n) {
+        // Write your code here.
+        //addition(n);
+        // if(n==0) return 0;
+        // return n+sumFirstN(n-1);
+        return (n*(n+1)/2);
+    }
+    // static long sum = 0L;
+    // static void addition(long a){
+    //     if(a<1) return;
+    //     addition(a-1);
+    //     sum+=a;
+
+    // }
+}
