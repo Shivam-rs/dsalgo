db7419166c32958ee4ccc25067e66810ad377e25
Shivam97.rs@gmail.com <crikfan5@gmail.com>
2023-12-02 19:47:02 -0500

    Hashing Using Array

--- java/strvr/basichashing/FreqArray.java
+++ java/strvr/basichashing/FreqArray.java
@@ -4,13 +4,13 @@ package strvr.basichashing;
 public class FreqArray {
     public static int[] countFrequency(int n, int x, int []nums){
         // Write your code here.
-        int[] arr = new int[n];
+        int[] a = new int[n];
 
         for(int i =0;i<n;i++){
             if(nums[i]<=n){
-                arr[nums[i]-1]++;
+                a[nums[i]-1]++;
             }
         }
-        return arr;
+        return a;
     }
 }
