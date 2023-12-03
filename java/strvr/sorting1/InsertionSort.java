b7268f2d73a7efb7be75d653fd4b376c84feaee5
Shivam97.rs@gmail.com <crikfan5@gmail.com>
2023-12-03 12:04:45 -0500

    Insertion Sort

---
+++ java/strvr/sorting1/InsertionSort.java
@@ -0,0 +1,17 @@
+package strvr.sorting1;
+//@link - https://www.codingninjas.com/studio/problems/insertion-sort_624381?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=PROBLEM
+//@Link - https://takeuforward.org/data-structure/insertion-sort-algorithm/
+public class InsertionSort {
+    public static void insertionSort(int[] arr, int size) {
+        //Your code goes here
+        for(int i = 0; i<size; i++){
+            for(int j = i; j>0; j--){
+                if(arr[j-1]>arr[j]){
+                    int temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+}
