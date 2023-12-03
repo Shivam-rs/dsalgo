2cc92abe0add83e71185533320e71d139a49a3fa
Shivam97.rs@gmail.com <crikfan5@gmail.com>
2023-12-03 09:48:23 -0500

    Selection Sort

---
+++ java/strvr/sorting1/SelectionSort.java
@@ -0,0 +1,21 @@
+package strvr.sorting1;
+
+//@Link - https://www.codingninjas.com/studio/problems/selection-sort_624469?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=PROBLEM
+//@link - https://takeuforward.org/sorting/selection-sort-algorithm/
+public class SelectionSort {
+    public static void selectionSort(int[] arr) {
+        //Your code goes here
+        int len = arr.length;
+        for(int i = 0; i<len-1; i++){
+            int min= i;
+            for(int j = i+1; j<len; j++){
+                if(arr[j]<arr[min]){
+                    min = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+}
