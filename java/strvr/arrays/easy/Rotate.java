c861a34e56c6f4e24d3fd4b6ad0bacaede797252
Shivam97.rs@gmail.com <crikfan5@gmail.com>
2023-12-14 22:01:27 -0500

    Rotating array left or right by k

--- java/strvr/arrays/easy/Rotate.java
+++ java/strvr/arrays/easy/Rotate.java
@@ -7,15 +7,24 @@ public class Rotate {
 
     public static void main(String[] args) {
         int[] arr = {9, 4, 7, 6, 3, 1, 5};
-        rotate(arr,3);
+        rotateRight(arr,3);
         System.out.println(Arrays.toString(arr));
     }
-    public static void rotate(int[] nums, int k) {
+    public static void rotateRight(int[] nums, int k) {
         int n = nums.length;
         k= k % n;
 
         reverse(nums, 0, n - k - 1);
         reverse(nums, n - k, n - 1);
+        reverse(nums, 0, n-1);
+    }
+
+    public static void rotateLeft(int[] nums, int k) {
+        int n = nums.length;
+        k= k % n;
+
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
         reverse(nums, 0, n-1);
     }
 
