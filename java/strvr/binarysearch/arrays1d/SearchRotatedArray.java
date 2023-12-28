9b645fb9b8d2e29ffc35985c40c3b94b14e3067b
Shivam97.rs@gmail.com <crikfan5@gmail.com>
2023-12-28 14:02:26 -0500

    Search Element in a Rotated Sorted Array

---
+++ java/strvr/binarysearch/arrays1d/SearchRotatedArray.java
@@ -0,0 +1,48 @@
+package strvr.binarysearch.arrays1d;
+//@Link - https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+//@Link - https://takeuforward.org/data-structure/search-element-in-a-rotated-sorted-array/
+public class SearchRotatedArray {
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{7, 8, 9, 1, 2, 3, 4, 5, 6};
+        int k = 1;
+        int ans = search(arr, k);
+        if (ans == -1)
+            System.out.println("Target is not present.");
+        else
+            System.out.println("The index is: " + ans);
+    }
+
+    public static int search(int[] nums, int target) {
+        int n = nums.length;
+        int low = 0, high = n-1;
+
+        while(low <= high){
+            int mid = (low + high)/2;
+            if(nums[mid] == target) return mid;
+
+            //Search for sorted half
+
+            if(nums[low] <= nums[mid]){ //<= because array can be of small size
+                //this means that left half is sorted
+                //Now we need to confirm is our target is here in sorted part or not
+                if(nums[low] <= target && target <=nums[mid] ){ //This would mean our target is somewhere in sorted part
+                    high = mid - 1;
+                }else{ //This would mean our target is in unsorted part, so we'll move low
+                    low = mid + 1;
+                }
+
+            }else{ //This would mean right part is sorted
+                if(nums[mid] <= target && target <=nums[high] ){ //This would mean our target is somewhere in sorted part
+                    low = mid + 1;
+                }else{ //This would mean our target is in unsorted part, so we'll move low
+                    high = mid - 1;
+                }
+            }
+
+        }
+
+        return -1;
+
+    }
+}
