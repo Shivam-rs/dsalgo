7ec53c9f1c243aa3eb09eb5a363d1864d63568be
Shivam97.rs@gmail.com <crikfan5@gmail.com>
2024-02-08 20:30:18 -0500

    Count 1st in binary signed/unsigned

---
+++ java/strvr/bitmanipulation/Count1s.java
@@ -0,0 +1,30 @@
+package strvr.bitmanipulation;
+
+//@Link - https://leetcode.com/problems/number-of-1-bits/solutions/4699803/java-beats-100-0ms-run-time-best-solution/
+
+public class Count1s {
+    public static void main(String[] args) {
+        int n = -3;
+        System.out.println(hammingWeight(n));
+    }
+
+    public static int hammingWeight(int n) {
+        int count = 0;
+
+        /* This code is for 1st approach
+        while (n != 0) {
+            count ++;
+            n = n&(n-1);
+        }
+        */
+
+        //This is for approach 2
+        while (n != 0) {
+            count += n & 1;
+            n >>>= 1;
+        }
+
+        return count;
+
+    }
+}
